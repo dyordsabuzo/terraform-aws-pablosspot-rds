4e82f125e8ead1a46c0b8488163a16ea6d257fde
George Abuzo <dyordsabuzo@gmail.com>
2022-03-11 16:20:18 +1100

    Fix final snapshot setup and space in password

--- output.tf
+++ output.tf
@@ -1,1 +1,15 @@
+output "dbhost" {
+  value = aws_ssm_parameter.dbhost.value
+}
 
+output "dbname" {
+  value = aws_ssm_parameter.dbname.value
+}
+
+output "dbuser" {
+  value = aws_ssm_parameter.dbuser.value
+}
+
+output "dbpassword_arn" {
+  value = aws_ssm_parameter.dbpassword.arn
+}
